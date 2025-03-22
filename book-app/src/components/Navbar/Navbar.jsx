1fbf74f7a6c9a787615315b3f0763b1d3a30670a
Hillary Robert <roberthillary30@gmail.com>
2025-03-22 13:37:44 +0200

    Added links to different pages and designed the nav bar with the styling

--- book-app/src/components/Navbar/Navbar.jsx
+++ book-app/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './Navbar.CSS'
+import {NavLink} from 'react-router-dom'
+
+
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='nav'>
+
+      
+      <img className='logo' src='./public/logo.png' alt="logo" /> 
+      
+
+      <ul className="nav-links">
+        <li><NavLink to={"/"}>Home</NavLink></li>
+        <li><NavLink to={"about"}>About</NavLink></li>
+        <li><NavLink to={"/favorites"}>Favorites</NavLink></li>
+      </ul>
+
+
+      <div className="auth">
+
+        <p>
+          <a href="">Sign in</a>
+        </p>
+
+        <button>Sign up</button>
+
+      </div>
+
+    </nav>  
   )
 }
 
