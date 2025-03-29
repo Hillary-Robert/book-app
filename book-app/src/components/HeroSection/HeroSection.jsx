26058c1d3a6c65cda09b511db7448d8e68497f1f
Hillary Robert <roberthillary30@gmail.com>
2025-03-29 11:12:05 +0200

    linked the API to the search input field

--- book-app/src/components/HeroSection/HeroSection.jsx
+++ book-app/src/components/HeroSection/HeroSection.jsx
@@ -1,39 +1,61 @@
-import React from 'react'
+import { useState } from 'react'
 import './HeroSection.css'
-import Navbar from '../Navbar/Navbar'
 
 export const HeroSection = () => {
+  const [query, setQuery] = useState('')
+  const [books, setBooks] = useState([])
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const handleSearch = async () => {
+    if (!query.trim()) 
+      
+      return setError('Please enter a book search')
+
+    try {
+      const response = await fetch(`https://openlibrary.org/search.json?q=${query}`)
+      if (!response.ok) throw new Error("Book search Failed")
+      const data = await response.json()
+      if (data.docs.length === 0) {
+        setError('No books found. Try another search.')
+      }
+
+      setBooks(data.docs)
+    } catch (error) {
+      setError('Something went wrong. Please try again.')
+      
+    } finally {
+      setLoading(false)
+    }
+  }
+  
+
   return (
+    <>
+      <section className='hero-section'>
+        <header>
+          <div className='title-text'>
+            <h1>Discover Your Next Favorite Book 📚</h1>
+            <p>Whether you're chasing a new adventure, revisiting an old favorite, or discovering a hidden gem — search, explore, and save the stories that move you.</p>
+          </div>
+
+          <div className='search-container'>
+            <input
+              type="text"
+              placeholder='Search by book title or author'
+              className='search-input'
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyDown={(e) => e.key === 'Enter' && handleSearch()}
+            />
+            <button className='search-button' onClick={handleSearch}>
+              Search
+            </button>
+          </div>
+        </header>
+      </section>
+
     
-    <section className='hero-section'>
-
-      <header>
-
-        <div className='title-text'>
-
-          <h1>Discover Your Next Favorite Book 📚</h1>
-
-          <p>Whether you're chasing a new adventure, revisiting an old favorite, or discovering a hidden gem — search, explore, and save the stories that move you, powered by Google Books.
-          </p>
-
-        </div>
-
-
-        <div className='search-container'>
-
-          <input type="text" placeholder='search by book, title, author name' className='search-input'/>
-
-          <button className='search-button '>Search</button>
-
-        </div>
-
-        
-        
-      </header>
-
-   
-
-
-    </section>
+    </>
   )
 }
