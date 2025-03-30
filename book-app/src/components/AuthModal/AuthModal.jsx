0ab901b98d13fadf3a3b6a67b955f41bbc2646a2
Hillary Robert <roberthillary30@gmail.com>
2025-03-30 22:38:21 +0300

    Added a pop up modal when Read is clicked on the details page

--- book-app/src/components/AuthModal/AuthModal.jsx
+++ book-app/src/components/AuthModal/AuthModal.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import './AuthModal.css'
 
-function AuthModal() {
+const AuthModal = ({ onClose }) => {
   return (
-    <div>AuthModal</div>
+    <div className="modal-backdrop">
+      <div className="modal-content">
+        <h2>Sign up or Log in</h2>
+        <p>You must be signed in to read this book.</p>
+
+        <div className="modal-buttons">
+          <button onClick={() => alert(`Can't sign up now, try again later`)}>Sign Up</button>
+          <button onClick={() => alert(`Can't Log in now, try again later`)}>Log In</button>
+        </div>
+
+        <button className="close-btn" onClick={onClose}>✖</button>
+      </div>
+    </div>
   )
 }
 
